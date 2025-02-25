ff0a6de087d2002de7438077de5d421080442d3d
KaterinaDubas98 <katerinamcqueen1@gmail.com>
2025-02-25 19:56:31 +0300

    HW01

---
+++ src/edu/training/jc/simple_output/StairsOutput_06.java
@@ -0,0 +1,14 @@
+package edu.training.jc.simple_output;
+
+public class StairsOutput_06 {
+
+	public static void main(String[] args) {
+		System.out.println("“To wisely live your life, you don't need to know much\n\n\t"
+				+ "Just remember two main rules for thebeginning:\n\n\t\t"
+				+ "You better starve, than eat whatever\n\n\t"
+				+ "And better be alone, than with whoever.”\n\n"
+				+ "Omar Khayyam,Rubaiyat");
+
+	}
+
+}
